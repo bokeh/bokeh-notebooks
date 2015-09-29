e897d5068ef3c8436d178ad6858fb34d14da9003
Sarah Bird <sarah@bonvaya.com>
2015-09-29 08:31:39 -0400

    Update interactions notebook

--- tutorial/utils.py
+++ tutorial/utils.py
@@ -207,3 +207,50 @@ def get_medal_data():
         data.append(new_item)
     medals = pd.io.json.json_normalize(data, 'medals', ['name', 'country'])
     return medals
+
+def get_custom_hover():
+    from bokeh.plotting import figure, output_file, show, ColumnDataSource
+    from bokeh.models import HoverTool
+
+    source = ColumnDataSource(
+            data=dict(
+                x=[1, 2, 3, 4, 5],
+                y=[2, 5, 8, 2, 7],
+                desc=['A', 'b', 'C', 'd', 'E'],
+                imgs = [
+                    'http://bokeh.pydata.org/static/snake.jpg',
+                    'http://bokeh.pydata.org/static/snake2.png',
+                    'http://bokeh.pydata.org/static/snake3D.png',
+                    'http://bokeh.pydata.org/static/snake4_TheRevenge.png',
+                    'http://bokeh.pydata.org/static/snakebite.jpg'
+                ]
+            )
+        )
+
+    hover = HoverTool(
+            tooltips="""
+            <div>
+                <div>
+                    <img
+                        src="@imgs" height="42" alt="@imgs" width="42"
+                        style="float: left; margin: 0px 15px 15px 0px;"
+                        border="2"
+                    ></img>
+                </div>
+                <div>
+                    <span style="font-size: 17px; font-weight: bold;">@desc</span>
+                    <span style="font-size: 15px; color: #966;">[$index]</span>
+                </div>
+                <div>
+                    <span style="font-size: 15px;">Location</span>
+                    <span style="font-size: 10px; color: #696;">($x, $y)</span>
+                </div>
+            </div>
+            """
+        )
+
+    p = figure(plot_width=300, plot_height=300, tools=[hover],
+            title="Mouse over the dots")
+
+    p.circle('x', 'y', size=20, source=source)
+    return p
